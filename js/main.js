1f4027e2d400419bd28564476df760ca0283b19d
Adam SOudani <asoudani@email.com>
2020-06-04 16:48:56 -0700

    fixed js issue to hide certain divs

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-
 // $(window).load(function(){
 
 // $('#page-loader').delay(300).fadeOut(400, function(){
@@ -11,56 +10,46 @@
 
 // });
 
-
-$('.home-link').on('click', function(){
-    $('.resume-body').css("display", "none");
-    $('.home-body').css("display", "block");
-    $('.contact-body').css("display", "none");
-    $('.portfolio-body').css("display", "none");
-    $('.skills-body').css("display", "none");
-})
-
-$('.skills-link').on('click', function(){
-    $('.resume-body').hide();
-    $('.home-body').hide();
-    $('.contact-body').hide();
-    $('.portfolio-body').hide();
-    $('.skills-body').show();
-})
-
-$('.contact-link').on('click', function(){
-    $('.resume-body').hide();
-    $('.home-body').hide();
-    $('.contact-body').show();
-    $('.portfolio-body').hide();
-    $('.skills-body').hide();
-})
-
-$('.portfolio-link').on('click', function(){
-    $('.resume-body').hide();
-    $('.home-body').hide();
-    $('.contact-body').hide();
-    $('.portfolio-body').show();
-    $('.skills-body').hide();
-})
-
-
-$('#travel').on('click', function(){
-    window.location = 'https://blue-project-2.herokuapp.com/';
-    
+$(".home-link").on("click", function () {
+  $(".home-body").css("display", "block");
+  $(".contact-form").css("display", "none");
+  $(".portfolio-container").css("display", "none");
+  $(".skills-body").css("display", "none");
 });
 
-$('#friend').on('click', function(){
-    window.location = 'https://calm-island-98205.herokuapp.com/';
-    
+$(".skills-link").on("click", function () {
+  $(".home-body").hide();
+  $(".contact-form").hide();
+  $(".portfolio-container").hide();
+  $(".skills-body").show();
 });
 
-$('#burger').on('click', function(){
-    window.location = 'https://nameless-sea-77335.herokuapp.com/';
-    
+$(".contact-link").on("click", function () {
+  $(".home-body").hide();
+  $(".contact-form").show();
+  $(".portfolio-container").hide();
+  $(".skills-body").hide();
 });
 
-$('#group').on('click', function(){
-    window.location = 'https://kittm.github.io/project-1/';
-    
+$(".portfolio-link").on("click", function () {
+  $(".home-body").hide();
+  $(".contact-form").hide();
+  $(".portfolio-container").show();
+  $(".skills-body").hide();
+});
+
+$("#travel").on("click", function () {
+  window.location = "https://blue-project-2.herokuapp.com/";
+});
+
+$("#friend").on("click", function () {
+  window.location = "https://calm-island-98205.herokuapp.com/";
+});
+
+$("#burger").on("click", function () {
+  window.location = "https://nameless-sea-77335.herokuapp.com/";
+});
+
+$("#group").on("click", function () {
+  window.location = "https://kittm.github.io/project-1/";
 });
