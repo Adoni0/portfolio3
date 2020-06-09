9ec1a92257fddd942720aacdc7df942d7767911b
Adam SOudani <asoudani@email.com>
2020-06-08 20:01:39 -0700

    percentages in skills div

--- js/main.js
+++ js/main.js
@@ -10,54 +10,56 @@
 
 // });
 
-$(".home-link").on("click", function () {
-  $(".home-body").css("display", "block");
-  $(".contact-form").css("display", "none");
-  $(".portfolio-container").css("display", "none");
-  $(".skills-body").css("display", "none");
-});
+$(document).ready(function () {
+  $(".home-link").on("click", function () {
+    $(".home-body").css("display", "block");
+    $(".contact-form").css("display", "none");
+    $(".portfolio-container").css("display", "none");
+    $(".skills-body").css("display", "none");
+  });
 
-$(".skills-link").on("click", function () {
-  $(".home-body").hide();
-  $(".contact-form").hide();
-  $(".portfolio-container").hide();
-  $(".skills-body").show();
-});
+  $(".skills-link").on("click", function () {
+    $(".home-body").hide();
+    $(".contact-form").hide();
+    $(".portfolio-container").hide();
+    $(".skills-body").show();
+  });
 
-$(".contact-link").on("click", function () {
-  $(".home-body").hide();
-  $(".contact-form").show();
-  $(".portfolio-container").hide();
-  $(".skills-body").hide();
-});
+  $(".contact-link").on("click", function () {
+    $(".home-body").hide();
+    $(".contact-form").show();
+    $(".portfolio-container").hide();
+    $(".skills-body").hide();
+  });
 
-$(".portfolio-link").on("click", function () {
-  $(".home-body").hide();
-  $(".contact-form").hide();
-  $(".portfolio-container").show();
-  $(".skills-body").hide();
-});
+  $(".portfolio-link").on("click", function () {
+    $(".home-body").hide();
+    $(".contact-form").hide();
+    $(".portfolio-container").show();
+    $(".skills-body").hide();
+  });
 
-$("#travel").on("click", function () {
-  window.location = "https://blue-project-2.herokuapp.com/";
-});
+  $("#travel").on("click", function () {
+    window.location = "https://blue-project-2.herokuapp.com/";
+  });
 
-$("#friend").on("click", function () {
-  window.location = "https://calm-island-98205.herokuapp.com/";
-});
+  $("#friend").on("click", function () {
+    window.location = "https://calm-island-98205.herokuapp.com/";
+  });
 
-$("#burger").on("click", function () {
-  window.location = "https://nameless-sea-77335.herokuapp.com/";
-});
+  $("#burger").on("click", function () {
+    window.location = "https://nameless-sea-77335.herokuapp.com/";
+  });
 
-$("#group").on("click", function () {
-  window.location = "https://kittm.github.io/project-1/";
-});
+  $("#group").on("click", function () {
+    window.location = "https://kittm.github.io/project-1/";
+  });
 
-$("#store").on("click", function () {
+  $("#store").on("click", function () {
     window.location = "https://github.com/Adoni0/node-mysql";
   });
 
   $("#train").on("click", function () {
     window.location = "https://adoni0.github.io/Train-Scheduler/";
   });
+});
